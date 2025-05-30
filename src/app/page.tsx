fbaaa27cc0f13a9b89e84a3b402394eeca7a2b8c
Picharyawee <picharyawee.crl@gmail.com>
2025-05-30 15:20:07 +0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Box, CssBaseline } from '@mui/material';
+import ChatPanel from '@/components/ChatPanel';
+import DocumentPanel from '@/components/DocumentPanel';
+import NotePanel from '@/components/NotePanel';
+import NavBar from '@/components/NavBar';
+
+export default function HomePage() {
+  return (
+    <>
+      <CssBaseline />
+
+      <Box 
+      display="flex" 
+      flexDirection="column" 
+      height="100vh"
+      >
+        <NavBar />
+
+        <Box 
+        display="flex" 
+        flexGrow={1} 
+        overflow="hidden"
+        >
+          <Box 
+          width="25%" 
+          height="100%" 
+          overflow="auto"
+          >
+            <DocumentPanel />
+          </Box>
+
+          <Box 
+          flexGrow={1} 
+          p={2} 
+          overflow="auto"
+          >
+            <ChatPanel />
+          </Box>
+
+          <Box 
+          width="25%" 
+          height="100%"
+          overflow="auto"
+          >
+            <NotePanel />
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { AppBar , Toolbar , Typography } from '@mui/material'
+
+export default function NavBar() {
+    return (
+        <AppBar 
+        position="static"
+        sx={{
+            backgroundColor: '#1a202c',
+            boxShadow: 2
+        }}
+        >
+            <Toolbar
+            sx={{
+                justifyContent: 'center'
+            }}
+            >
+                <Typography
+                variant="h4"
+                fontWeight={"bold"}
+                color="white"
+                >
+                    LarnGaer LM
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
