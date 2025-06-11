68fa55857798b8a0dc12593bcc3677fdf1cd1dc4
LapisBerry <kittiphopkhankaew@gmail.com>
2025-06-11 12:11:33 +0700

    feat: resourceContext available

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import ChatPanel from '@/components/ChatPanel';
 import ResourcePanel from '@/components/ResourcePanel';
 import NotePanel from '@/components/NotePanel';
 import Header from '@/components/Header';
+import { ResourceProvider } from '@/contexts/ResourceContext';
 
 export default function HomePage() {
   return (
-    <>
+    <ResourceProvider>
       <CssBaseline />
       <Header />
-      <Box 
-        display="flex" 
+      <Box
+        display="flex"
         overflow="hidden"
         height="calc(100vh - 100px)"
       >
@@ -19,6 +20,6 @@ export default function HomePage() {
         <ChatPanel />
         <NotePanel />
       </Box>
-    </>
+    </ResourceProvider>
   );
 }
