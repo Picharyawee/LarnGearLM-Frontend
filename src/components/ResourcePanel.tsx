dca28c4950c5d60875be305bc7824e56a45c4037
LapisBerry <kittiphopkhankaew@gmail.com>
2025-06-24 18:46:02 +0700

    fix: max width box on resource panel

--- src/components/ResourcePanel.tsx
+++ src/components/ResourcePanel.tsx
@@ -155,7 +155,7 @@ export default function ResourcePanel() {
           flexGrow={1}
           sx={{
             overflowY: 'scroll',
-            maxWidth: '400px'
+            maxWidth: '800px'
           }}
         >
           <Typography 
