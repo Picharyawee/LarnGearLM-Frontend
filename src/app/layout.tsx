9fa223952d0afb46e5ff1a0ecd93061aec851730
LapisBerry <kittiphopkhankaew@gmail.com>
2025-06-24 12:02:59 +0700

    fix: use client and NoteProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ResourceProvider } from "@/lib/contexts/ResourceContext";
+import { NoteProvider } from "@/lib/contexts/NoteContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ResourceProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <NoteProvider>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+          </body>
+        </NoteProvider>
       </ResourceProvider>
     </html>
   );
